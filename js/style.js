0ad3f9499eb5e4c706ddb2e3e781adda204b9e9e
Gunmo Lee <77503982+rjsah5676@users.noreply.github.com>
2025-01-09 10:59:19 +0900

    로그인 페이지 제작

--- js/style.js
+++ js/style.js
@@ -7,11 +7,27 @@ function getStyles() {
     ft.innerHTML = `Copyright 2024. 
     <a href="https://github.com/rjsah5676/Front-Practice" 
     style="text-decoration:none; color:#a2a2a2;">이건모.</a> All Right Reserved.`;
-    ft.style.widdth="100%";
+    ft.style.width="100%";
     ft.style.height="60px";
     ft.style.lineHeight="60px";
     ft.style.color="#a2a2a2";
     ft.style.fontSize="20px";
     ft.style.zIndex="-1";
     ft.style.textAlign="center";
+    const media = matchMedia("screen and (max-width: 1024px)");
+
+    media.addListener((a) => {
+        if(a.matches) {
+            ft.style.fontSize = "15px";
+        }
+        else {
+            ft.style.fontSize = "20px";
+        }
+    });
+
+    document.addEventListener('DOMContentLoaded', () => {
+        window.setTimeout(() => {
+          document.body.classList.remove('fade');
+        });
+      });
 }
