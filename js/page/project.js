d941a3a54781080ed03e8780de1ac4048270d0cd
Gunmo Lee <77503982+rjsah5676@users.noreply.github.com>
2025-01-13 14:43:57 +0900

    project page

--- js/page/project.js
+++ js/page/project.js
@@ -1,3 +1,5 @@
+var page=1;
+
 function clickPersonal(){
     let pbutton = document.getElementById("personal-button");
     let tbutton = document.getElementById("team-button");
@@ -34,4 +36,39 @@ function clickTeam(){
     tbutton.style.backgroundColor='white';
     tbutton.style.color='#121212';
     tbutton.style.fontWeight=600;
+}
+
+function openImg(a,b){
+    let il=document.querySelector('#img-list-'+b);
+    let preimg = document.querySelector("#img-list-"+b+" .banner_image:nth-child("+page+")");
+    page=1;
+    document.querySelector("#img-list-"+b+" .banner_image:nth-child(1)").style.opacity=a;
+    il.style.opacity=a;
+    if(a==1) {
+        il.style.zIndex=9;
+    }
+    if(a==0) {
+        preimg.style.opacity=0;
+        il.style.zIndex=-1;
+    }
+}
+
+function leftImage(w,pg){
+    let preimg = document.querySelector("#img-list-"+w+" .banner_image:nth-child("+page+")");
+    preimg.style.opacity=0;
+    if(page==1) page=pg;
+    else --page;
+    console.log(page);
+    let img = document.querySelector("#img-list-"+w+" .banner_image:nth-child("+page+")");
+    img.style.opacity=1;
+}
+
+function rightImage(w,pg){
+    let preimg = document.querySelector("#img-list-"+w+" .banner_image:nth-child("+page+")");
+    preimg.style.opacity=0;
+    if(page==pg) page=1;
+    else ++page;
+    console.log(page);
+    let img = document.querySelector("#img-list-"+w+" .banner_image:nth-child("+page+")");
+    img.style.opacity=1;
 }
